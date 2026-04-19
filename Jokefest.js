9ebecd7e92770480f818e079cc5747eb51cdb4d2
rajeevsharmap <rajsharma08032007@gmail.com>
2026-04-20 00:49:10 +0530

    Adding Fitler Functionality and optimizing the resposiveness on smaller screen

--- Jokefest.js
+++ Jokefest.js
@@ -1,11 +1,44 @@
+let jokeCounter = document.querySelector('.jokeCounter');
+let statsMessage = document.querySelector('.JokeStatsMessage');
+let count =0;
+
+jokeCounter.innerText= count;
+statsMessage.innerText = "🎯Ready!";
+
 document.querySelector('.joke-request').addEventListener('click', async () => {
     const jokeDisplay = document.querySelector('.jokeDisplayBox');
     jokeDisplay.innerHTML = '';
+
+    const checkedCategories = [...document.querySelectorAll('.jokeCategories input[type="checkbox"]:checked')]
+        .map(cb => cb.name);
+        const categoryString = checkedCategories.length > 0
+        ? checkedCategories.map(c => checkedCategories[c] || c).join(',')
+        : 'Any';
+
+    const checkedTypes = [...document.querySelectorAll('.jokeTypes input[type="checkbox"]:checked')]
+        .map(cb => cb.name);
+
+    let typeParam = '';
+    if (checkedTypes.length === 1) {
+        typeParam = `?type=${checkedTypes[0]}`;
+    }
+    const apiURL = `https://v2.jokeapi.dev/joke/${categoryString}${typeParam}`;
+    
     try {
-        const response = await fetch('https://v2.jokeapi.dev/joke/Any');
+        const response = await fetch(apiURL);
         if (!response.ok) throw new Error(`Request Failed to connect with Server, ${response.status}`);
         const data = await response.json();
-        console.log(data);
+
+        count +=1;
+        jokeCounter.innerText = count;
+
+        if(count <= 5){
+            statsMessage.innerText= "🔥 Warming Up!";
+        }else if(count < 10){
+            statsMessage.innerText= "😂 On Fire!";
+        }else{
+            statsMessage.innerText= "🏆 Joke Master!";
+        }
 
         if (data.type === "single") {
             jokeDisplay.innerHTML =
@@ -21,7 +54,7 @@ document.querySelector('.joke-request').addEventListener('click', async () => {
                             <div class="jokeId">JokeId: #${data.id}</div>
                             <div class="JokeLike">
                                 <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="m480-120-58-52q-101-91-167-157T150-447.5Q111-500 95.5-544T80-634q0-94 63-157t157-63q52 0 99 22t81 62q34-40 81-62t99-22q94 0 157 63t63 157q0 46-15.5 90T810-447.5Q771-395 705-329T538-172l-58 52Zm0-108q96-86 158-147.5t98-107q36-45.5 50-81t14-70.5q0-60-40-100t-100-40q-47 0-87 26.5T518-680h-76q-15-41-55-67.5T300-774q-60 0-100 40t-40 100q0 35 14 70.5t50 81q36 45.5 98 107T480-228Zm0-273Z"/></svg>
-                                LIKE
+                                <div class ="likeChange">LIKE</div>
                             </div>
                             <div class="jokeShare">
                                 <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="M680-80q-50 0-85-35t-35-85q0-6 3-28L282-392q-16 15-37 23.5t-45 8.5q-50 0-85-35t-35-85q0-50 35-85t85-35q24 0 45 8.5t37 23.5l281-164q-2-7-2.5-13.5T560-760q0-50 35-85t85-35q50 0 85 35t35 85q0 50-35 85t-85 35q-24 0-45-8.5T598-672L317-508q2 7 2.5 13.5t.5 14.5q0 8-.5 14.5T317-452l281 164q16-15 37-23.5t45-8.5q50 0 85 35t35 85q0 50-35 85t-85 35Zm0-80q17 0 28.5-11.5T720-200q0-17-11.5-28.5T680-240q-17 0-28.5 11.5T640-200q0 17 11.5 28.5T680-160ZM200-440q17 0 28.5-11.5T240-480q0-17-11.5-28.5T200-520q-17 0-28.5 11.5T160-480q0 17 11.5 28.5T200-440Zm508.5-291.5Q720-743 720-760t-11.5-28.5Q697-800 680-800t-28.5 11.5Q640-777 640-760t11.5 28.5Q663-720 680-720t28.5-11.5ZM680-200ZM200-480Zm480-280Z"/></svg>
@@ -61,7 +94,7 @@ document.querySelector('.joke-request').addEventListener('click', async () => {
                                 <div class="jokeId">JokeId: #${data.id}</div>
                                 <div class="JokeLike">
                                     <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="m480-120-58-52q-101-91-167-157T150-447.5Q111-500 95.5-544T80-634q0-94 63-157t157-63q52 0 99 22t81 62q34-40 81-62t99-22q94 0 157 63t63 157q0 46-15.5 90T810-447.5Q771-395 705-329T538-172l-58 52Zm0-108q96-86 158-147.5t98-107q36-45.5 50-81t14-70.5q0-60-40-100t-100-40q-47 0-87 26.5T518-680h-76q-15-41-55-67.5T300-774q-60 0-100 40t-40 100q0 35 14 70.5t50 81q36 45.5 98 107T480-228Zm0-273Z"/></svg>
-                                    LIKE
+                                    <div class ="likeChange">LIKE</div>
                                 </div>
                                 <div class="jokeShare">
                                         <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="M680-80q-50 0-85-35t-35-85q0-6 3-28L282-392q-16 15-37 23.5t-45 8.5q-50 0-85-35t-35-85q0-50 35-85t85-35q24 0 45 8.5t37 23.5l281-164q-2-7-2.5-13.5T560-760q0-50 35-85t85-35q50 0 85 35t35 85q0 50-35 85t-85 35q-24 0-45-8.5T598-672L317-508q2 7 2.5 13.5t.5 14.5q0 8-.5 14.5T317-452l281 164q16-15 37-23.5t45-8.5q50 0 85 35t35 85q0 50-35 85t-85 35Zm0-80q17 0 28.5-11.5T720-200q0-17-11.5-28.5T680-240q-17 0-28.5 11.5T640-200q0 17 11.5 28.5T680-160ZM200-440q17 0 28.5-11.5T240-480q0-17-11.5-28.5T200-520q-17 0-28.5 11.5T160-480q0 17 11.5 28.5T200-440Zm508.5-291.5Q720-743 720-760t-11.5-28.5Q697-800 680-800t-28.5 11.5Q640-777 640-760t11.5 28.5Q663-720 680-720t28.5-11.5ZM680-200ZM200-480Zm480-280Z"/></svg>
@@ -71,6 +104,17 @@ document.querySelector('.joke-request').addEventListener('click', async () => {
                         </div>
                     `;
         }
+
+        let like =document.querySelector('.JokeLike');
+        let likeChanger =document.querySelector('.likeChange');
+
+        like.addEventListener('click', ()=>{
+            if(likeChanger.innerText == "LIKED"){
+                likeChanger.innerText = "LIKE";
+            }else{
+                likeChanger.innerText="LIKED";
+            }
+        });
     }
     catch (error) {
         jokeDisplay.innerHTML = `<p class='error'>Error Handled , Try later <br> ${error.message}<p>`;
